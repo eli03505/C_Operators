d485f01160e53a26b26c81311fc1abbb3b3dbce3
Ibad_3qug <Ibad_3qug@itstep.edu.az>
2021-04-16 22:22:11 +0300

    Добавьте файлы проекта.

---
+++ C_Operators/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Operators
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers: ");
+            bool calculator = int.TryParse(Console.ReadLine(), out int num);
+            Console.WriteLine("Write Operator: ");
+
+            bool calc = char.TryParse(Console.ReadLine(), out char Number);
+            Console.WriteLine("Enter the second number: ");
+
+            bool numbers = int.TryParse(Console.ReadLine(), out int num2);
+
+            if (calculator && numbers && calc)
+            {
+                switch (Number)
+                {
+                    case '*':
+                        {
+                            Console.WriteLine(num * num2);
+                            break;
+                        }
+                    case '/':
+                        {
+                            Console.WriteLine(num / num2);
+                            break;
+                        }
+                    case '-':
+                        {
+                            Console.WriteLine(num - num2);
+                            break;
+                        }
+                    case '+':
+                        {
+                            Console.WriteLine(num + num2);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Invalid argument");
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid argument");
+            }
+        }
+    }
+}
